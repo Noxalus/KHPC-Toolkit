e9b148f634fb0381c40cfa12e8d0a2634cdb97b7
Noxalus <noxalus@gmail.com>
2021-04-24 19:39:53 +0200

    Make sure SCD header contain accurate info of the audio ADPCM data and don't change the sample rate anymore (so I removed SoX from the project).

--- SCDEncoder/SCDEncoder/Program.cs
+++ SCDEncoder/SCDEncoder/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NAudio.Wave;
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -10,6 +11,9 @@ namespace SCDEncoder
     {
         private static readonly string TOOLS_PATH = Path.Combine(Path.GetDirectoryName(AppContext.BaseDirectory), "tools");
         private static readonly string SCD_HEADER_FILE = Path.Combine(Path.GetDirectoryName(AppContext.BaseDirectory), "scd/header.scd");
+
+        private const string TMP_FOLDER_NAME = "tmp";
+        private const string ADPCM_SUFFIX = "-adpcm";
 
         static void Main(string[] args)
         {
@@ -26,16 +30,9 @@ namespace SCDEncoder
                     return;
                 }
 
-                if (!File.Exists(@$"{TOOLS_PATH}/sox/sox.exe"))
-                {
-                    Console.WriteLine($"Please put sox.exe in the tools folder: {TOOLS_PATH}/sox");
-                    Console.WriteLine("You can find it here: https://sourceforge.net/projects/sox/files/sox/");
-                    return;
-                }
-
                 if (!File.Exists(@$"{TOOLS_PATH}/adpcmencode/adpcmencode3.exe"))
                 {
-                    Console.WriteLine($"Please put sox.exe in the tools folder: {TOOLS_PATH}/adpcmencode");
+                    Console.WriteLine($"Please put adpcmencode3.exe in the tools folder: {TOOLS_PATH}/adpcmencode");
                     Console.WriteLine("You can find it in the Windows 10 SDK: https://developer.microsoft.com/fr-fr/windows/downloads/windows-10-sdk/");
                     return;
                 }
@@ -64,7 +61,7 @@ namespace SCDEncoder
 
         private static void ConvertFile(string inputFile, string outputFolder)
         {
-            var tmpFolder = Path.Combine(Path.GetDirectoryName(AppContext.BaseDirectory), "tmp");
+            var tmpFolder = Path.Combine(Path.GetDirectoryName(AppContext.BaseDirectory), TMP_FOLDER_NAME);
 
             if (!Directory.Exists(tmpFolder))
                 Directory.CreateDirectory(tmpFolder);
@@ -72,8 +69,7 @@ namespace SCDEncoder
                 Directory.CreateDirectory(outputFolder);
 
             var wavPCMPath = Path.Combine(tmpFolder, $"{Path.GetFileNameWithoutExtension(inputFile)}.wav");
-            var wavPCM48Path = Path.Combine(tmpFolder, $"{Path.GetFileNameWithoutExtension(inputFile)}-48000Hz.wav");
-            var wavADPCMPath = Path.Combine(tmpFolder, $"{Path.GetFileNameWithoutExtension(inputFile)}-adpcm.wav");
+            var wavADPCMPath = Path.Combine(tmpFolder, $"{Path.GetFileNameWithoutExtension(inputFile)}{ADPCM_SUFFIX}.wav");
             var scdPath = Path.Combine(tmpFolder, $"{Path.GetFileNameWithoutExtension(inputFile)}.scd");
             var outputFile = Path.Combine(outputFolder, $"{Path.GetFileNameWithoutExtension(inputFile)}.win32.scd");
 
@@ -94,17 +90,9 @@ namespace SCDEncoder
                 File.Copy(inputFile, wavPCMPath, true);
             }
 
-            // Convert WAV PCM (any sample rate) to WAV PCM with a sample rate of 48kHz
-            p.StartInfo.FileName = $@"{TOOLS_PATH}/sox/sox.exe";
-            p.StartInfo.Arguments = $"\"{wavPCMPath}\" --rate 48000 \"{wavPCM48Path}\"";
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.RedirectStandardInput = false;
-            p.Start();
-            p.WaitForExit();
-
             // Convert WAV PCM into WAV MS-ADPCM
             p.StartInfo.FileName = $@"{TOOLS_PATH}/adpcmencode/adpcmencode3.exe";
-            p.StartInfo.Arguments = $"-b 32 \"{wavPCM48Path}\" \"{wavADPCMPath}\"";
+            p.StartInfo.Arguments = $"-b 32 \"{wavPCMPath}\" \"{wavADPCMPath}\"";
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardInput = false;
             p.Start();
@@ -123,9 +111,51 @@ namespace SCDEncoder
 
         private static void CreateSCD(string wavFilePath, string outputFile)
         {
-            var filename = Path.GetFileName(wavFilePath);
-            var wavData = File.ReadAllBytes(wavFilePath);
+            var streamName = Path.GetFileNameWithoutExtension(wavFilePath).Replace("-adpcm", "");
+            var wavData = StripWavHeader(File.ReadAllBytes(wavFilePath));
+            var waveFile = new WaveFileReader(wavFilePath);
+            var dummyScd = File.ReadAllBytes(SCD_HEADER_FILE);
 
+            using (var writer = new BinaryWriter(new MemoryStream(dummyScd)))
+            {
+                var totalFileSize = dummyScd.Length + wavData.Length;
+
+                // Total file size
+                writer.BaseStream.Position = 0x10;
+                writer.Write(totalFileSize);
+
+                // Stream name
+                writer.BaseStream.Position = 0x150;
+                writer.Write(Encoding.UTF8.GetBytes(streamName));
+
+                // Audio data size
+                writer.BaseStream.Position = 0x250;
+                writer.Write(wavData.Length);
+
+                // Channel count
+                writer.BaseStream.Position = 0x254;
+                writer.Write((uint)waveFile.WaveFormat.Channels);
+
+                // Sample rate
+                writer.BaseStream.Position = 0x258;
+                writer.Write((uint)waveFile.WaveFormat.SampleRate);
+
+                // Frame size / Block align
+                writer.BaseStream.Position = 0x27C;
+                writer.Write((short)waveFile.WaveFormat.BlockAlign);
+            }
+
+            waveFile.Close();
+
+            var finalScd = new byte[dummyScd.Length + wavData.Length];
+            Array.Copy(dummyScd, finalScd, dummyScd.Length);
+            Array.Copy(wavData, 0, finalScd, dummyScd.Length, wavData.Length);
+
+            File.WriteAllBytes(outputFile, finalScd);
+        }
+
+        private static byte[] StripWavHeader(byte[] wavData)
+        {
             // Find wave header size
             var pattern = Encoding.ASCII.GetBytes("data");
             var wavHeaderOffset = SearchBytePattern(wavData, pattern) + pattern.Length;
@@ -136,30 +166,7 @@ namespace SCDEncoder
             // Strip wav header
             wavData = wavData.Skip(wavHeaderOffset).ToArray();
 
-            byte[] dummyScd = File.ReadAllBytes(SCD_HEADER_FILE);
-
-            using (var writer = new BinaryWriter(new MemoryStream(dummyScd)))
-            {
-                var finalFileLength = dummyScd.Length + wavData.Length;
-
-                //file length
-                writer.BaseStream.Position = 0x10;
-                writer.Write(finalFileLength);
-
-                // filename
-                writer.BaseStream.Position = 0x150;
-                writer.Write(Encoding.UTF8.GetBytes(filename));
-
-                // Audio file data size
-                writer.BaseStream.Position = 0x250;
-                writer.Write(wavData.Length);
-            }
-
-            var finalScd = new byte[dummyScd.Length + wavData.Length];
-            Array.Copy(dummyScd, finalScd, dummyScd.Length);
-            Array.Copy(wavData, 0, finalScd, dummyScd.Length, wavData.Length);
-
-            File.WriteAllBytes(outputFile, finalScd);
+            return wavData;
         }
 
         private static int SearchBytePattern(byte[] data, byte[] pattern)
