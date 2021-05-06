de53d9e8b46d16685fe4d255c35efb4ad94053c4
Noxalus <noxalus@gmail.com>
2021-05-06 20:18:52 +0200

    Create a VAG extractor that can extract any VAG files from any input file.

---
+++ SCDEncoder/VAGExtractor/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace VAGExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                var inputFile = args[0];
+                var outputFolder = args.Length > 1 ? args[1] : "output";
+
+                if (!Directory.Exists(outputFolder))
+                    Directory.CreateDirectory(outputFolder);
+
+                var inputData = File.ReadAllBytes(inputFile);
+                var inputStream = File.OpenRead(inputFile);
+                var foundOffsets = SearchPatterns(inputData, "VAGp");
+
+                foreach (var offset in foundOffsets)
+                {
+                    inputStream.Seek(offset, SeekOrigin.Begin);
+
+                    var vag = new VAG(inputStream);
+
+                    Console.WriteLine($"{vag.Name}");
+                    
+                    vag.Export(outputFolder);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("VAGExtractor <file/dir> [<output dir>]");
+            }
+        }
+        
+        private static List<int> SearchPatterns(byte[] data, string stringPattern)
+        {
+            var foundOffsets = new List<int>();
+
+            var pattern = Encoding.ASCII.GetBytes(stringPattern);
+            int patternLength = pattern.Length;
+            int totalLength = data.Length;
+            byte firstMatchByte = pattern[0];
+            for (int i = 0; i < totalLength; i++)
+            {
+                if (firstMatchByte == data[i] && totalLength - i >= patternLength)
+                {
+                    byte[] match = new byte[patternLength];
+                    Array.Copy(data, i, match, 0, patternLength);
+                    if (match.SequenceEqual<byte>(pattern))
+                    {
+                        foundOffsets.Add(i);
+                    }
+                }
+            }
+
+            return foundOffsets;
+        }
+    }
+}
---
+++ SCDEncoder/VAGExtractor/VAG.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xe.BinaryMapper;
+
+namespace VAGExtractor
+{
+    class VAG
+    {
+        private const string MAGIC_CODE = "VAGp";
+
+        public class VAGHeader
+        {
+            [Data(Count = 4)] public string Magic { get; set; }
+            [Data] public UInt32 Version { get; set; }
+            [Data] public UInt32 ReservedArea1 { get; set; }
+            [Data] public UInt32 DataSize { get; set; } // In bytes
+            [Data] public UInt32 SampleRate { get; set; }
+            [Data(Count = 10)] public byte[] ReservedArea2 { get; set; }
+            [Data] public byte Channels { get; set; }
+            [Data] public byte ReservedArea3 { get; set; }
+            [Data(Count = 16)] public string Name { get; set; }
+        }
+
+        private VAGHeader _header;
+        private byte[] _audioData;
+
+        private IBinaryMapping _mapper;
+
+        public string Name => _header.Name;
+
+        public VAG(Stream stream)
+        {
+            // VAG files are in Big Endian...
+            _mapper = MappingConfiguration
+                .DefaultConfiguration(Encoding.UTF8, true)
+                .Build();
+
+            // Read base SCD header
+            _header = _mapper.ReadObject<VAGHeader>(stream);
+
+            _audioData = new byte[_header.DataSize];
+             stream.Read(_audioData);
+
+            if (!_header.Magic.Equals(MAGIC_CODE))
+            {
+                throw new Exception("Magic code not found, invalid SCD file.");
+            }
+        }
+
+        public void Export(string outputPath)
+        {
+            var fileStream = File.Create(Path.Combine(outputPath, $"{_header.Name}.vag"));
+
+            _mapper.WriteObject<VAGHeader>(fileStream, _header);
+            fileStream.Write(_audioData);
+        }
+    }
+}
